11464f85df4d3a9dc1c65e04e717289ad8a2ab4a
skies-starred <github@starred.foo>
2026-09-15 21:25:46 +0100

    internal: build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,58 +1,42 @@
-@file:Suppress("UnstableApiUsage")
-
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.loom)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.fletchingTable)
 }
 
-val ver = stonecutter.current.version
-val modId = project.property("mod.id").toString()
-val modName = project.property("mod.name").toString()
-val modVer = project.property("mod.version").toString()
+val catalogs = extensions.getByType<VersionCatalogsExtension>()
+val minecraft = stonecutter.current.version
+val lib = catalogs.named("libs${minecraft.replace(".", "")}")
+val mod = catalogs.named("mod")
 
-version = "$modVer+$ver"
-base.archivesName = modId
+version = "${mod("version")}+$minecraft"
+group = mod("group")
+base.archivesName = mod("id")
 
 repositories {
-    fun strictMaven(url: String, vararg groups: String) = maven(url) { content { groups.forEach(::includeGroupAndSubgroups) } }
-
-    strictMaven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1", "me.djtheredstoner")
-    strictMaven("https://api.modrinth.com/maven", "maven.modrinth")
-    strictMaven("https://maven.parchmentmc.org/", "org.parchmentmc")
-    strictMaven("https://jitpack.io", "com.github.stivais", "com.github.odtheking", "com.github.sivthepolarfox", "com.github.skies-starred")
+    maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
+    maven("https://api.modrinth.com/maven")
+    maven("https://jitpack.io")
 
     maven("https://maven.starred.foo/releases")
     maven("https://maven.starred.foo/snapshots")
 }
 
-fletchingTable {
-    mixins.create("main", Action {
-        mixin("default", "$modId.mixins.json") {
-            env("CLIENT")
-        }
-    })
-}
-
 dependencies {
-    minecraft("com.mojang:minecraft:$ver")
+    minecraft(lib["minecraft"])
 
-    localRuntime("devauth".global)
-    compileOnly("firmament".versioned)
+    localRuntime(libs.devauth)
 
-    implementation("fabric-api".versioned)
-    implementation("fabric-loader".global)
-    implementation("fabric-language-kotlin".global)
+    implementation(lib["odin"])
+    implementation(lib["fabric-api"])
+    implementation(libs.fabric.loader)
+    implementation(libs.fabric.language.kotlin)
+    implementation(libs.lwjgl.nanovg)
+    implementation(libs.commodore)
 
-    implementation("odin-prod".versioned)
-    implementation("commodore".global)
-    implementation("lwjgl-nanovg".global)
-
-    shadow("snowbird".versioned)
-    shadow("kommand".global)
+    shadow(libs.kommand)
+    shadow(lib["snowbird"])
+    shadow(lib["cascade"])
+    shadow(lib["updater"])
 }
 
 loom {
@@ -60,12 +44,7 @@ loom {
 
     runConfigs.named("client") {
         generateRunConfig = true
-        jvmArguments.addAll(
-            "-Ddevauth.enabled=true",
-            "-Ddevauth.account=main",
-            "-XX:+AllowEnhancedClassRedefinition",
-            "-XX:+IgnoreUnrecognizedVMOptions",
-        )
+        jvmArguments.addAll("-Ddevauth.enabled=true", "-Ddevauth.account=main", "-XX:+AllowEnhancedClassRedefinition", "-XX:+IgnoreUnrecognizedVMOptions")
     }
 
     runConfigs.named("server") {
@@ -73,34 +52,20 @@ loom {
     }
 }
 
-tasks.withType<JavaCompile>().configureEach {
-    options.release.set(25)
-}
-
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(25)
     withSourcesJar()
 }
 
 kotlin {
-    jvmToolchain(25)
-
     compilerOptions {
-        jvmTarget.set(JvmTarget.valueOf("JVM_25"))
-
-        freeCompilerArgs.addAll("-XXLanguage:+ExplicitBackingFields", "-Xcontext-parameters", "-Xcontext-sensitive-resolution", "-Xlambdas=class")
+        freeCompilerArgs.addAll("-Xcontext-sensitive-resolution", "-Xcollection-literals", "-Xskip-prerelease-check")
         optIn.add("kotlin.time.ExperimentalTime")
     }
 }
 
 tasks {
     processResources {
-        val r = mapOf(
-            "id" to modId,
-            "name" to modName,
-            "version" to modVer,
-            "minecraft" to project.property("mod.mc_dep")
-        )
+        val r = mapOf("id" to mod("id"), "name" to mod("name"), "version" to mod("version"), "minecraft" to lib("compatibility"))
 
         inputs.properties(r)
         filesMatching("fabric.mod.json") { expand(r) }
@@ -110,20 +75,22 @@ tasks {
         description = "Builds and collects mod jars."
         group = "build"
         from(jar, kotlinSourcesJar)
-        into(rootProject.layout.buildDirectory.file("libs/${project.property("mod.version")}"))
+        into(rootProject.layout.buildDirectory.file("libs/${mod("version")}"))
         dependsOn("build")
     }
 }
-
-val String.global: Provider<MinimalExternalModuleDependency>
-    get() = extensions.getByType<VersionCatalogsExtension>().named("libs").findLibrary(this).get()
-
-val String.versioned: Provider<MinimalExternalModuleDependency>
-    get() = extensions.getByType<VersionCatalogsExtension>().named("libs").findLibrary("$this-${ver.replace(".", "_")}").get()
 
 fun DependencyHandlerScope.shadow(dep: Any, config: ExternalModuleDependency.() -> Unit = {}) {
     val d = create((dep as? Provider<*>)?.get() ?: dep) as ExternalModuleDependency
     d.config()
     include(d)
     implementation(d)
+}
+
+operator fun VersionCatalog.get(name: String): Provider<MinimalExternalModuleDependency> {
+    return findLibrary(name).get()
+}
+
+operator fun VersionCatalog.invoke(name: String): String {
+    return findVersion(name).get().requiredVersion
 }
--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -5,8 +5,21 @@ plugins {
 
 stonecutter active "26.1"
 
+val mod = extensions.getByType<VersionCatalogsExtension>().named("mod")
+
 stonecutter parameters {
-    swaps["mod_version"] = "\"" + property("mod.version") + "\""
-    swaps["mod_id"] = "\"" + property("mod.id") + "\""
-    swaps["minecraft"] = "\"" + node.metadata.version + "\""
+    swaps["mod_version"] = "\"${mod("version")}\""
+    swaps["mod_id"] = "\"${mod("id")}\""
+    swaps["mod_name"] = "\"${mod("name")}\""
+    swaps["minecraft"] = "\"${node.metadata.version}\""
+
+    replacements {
+        string(current.parsed >= "26.2") {
+            replace("client.screen", "client.gui.screen()")
+        }
+    }
+}
+
+operator fun VersionCatalog.invoke(name: String): String {
+    return findVersion(name).get().requiredVersion
 }
